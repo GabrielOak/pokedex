31d60e073d3d9342e86cbf9c671d3da0ef2768c2
Gabriel Carvalho <gabriel315df@gmail.com>
2022-05-18 15:12:00 -0300

    Add styled component and navigation configuration

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Home = () => (
+  <S.Wrapper>
+    <S.Header>Home</S.Header>
+  </S.Wrapper>
+);
+
+export default Home;
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Wrapper = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.Text`
+  font-size: 14px;
+  color: black;
+`;
